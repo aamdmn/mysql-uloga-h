cbef2de06b8c3a19ea9c1c4ba451eb9352bebcaa
Adam Demian <adamdemian1@gmail.com>
2024-01-11 19:09:35 +0100

    Delete form table and related files

--- drizzle/0000_milky_butterfly.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `form` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`name` char,
-	`email` char,
-	`phone` int,
-	`message` char,
-	`person` boolean DEFAULT true,
-	CONSTRAINT `form_id` PRIMARY KEY(`id`)
-);
